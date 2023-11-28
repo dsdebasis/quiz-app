3e82e11e61c4690756cf46970a90a9ad5179f93d
Debasis Sahoo <dsdebasis2@gmail.com>
2023-11-29 00:11:41 +0530

    quiz app

---
+++ index.js
@@ -0,0 +1,16 @@
+let question = document.getElementById('a');
+let score = document.getElementById('b');
+let btn = document.querySelector('button');
+const opn = document.querySelectorAll('h4');
+const parent = document.getElementById('parent');
+console.log(opn)
+
+let answers = ['Nirmala Sita Raman','RBI','SEBI','IRDAI','Delhi','Gross Domestic Product','S.Jaysankar','Elon Musk','API','IP'  ]
+
+parent.addEventListener('click',function abc(e){
+   
+})
+
+
+
+
